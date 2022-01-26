c4cdd0fb1b0a89b2333068dc2955437aa60a8e40
Joseph M <49419769+MoriZwei@users.noreply.github.com>
2022-01-26 14:51:25 -0500

    Update ListManager.h
    Updated to 2.1

--- SourceCode/PlayVideo/ListManager.h
+++ SourceCode/PlayVideo/ListManager.h
@@ -2,7 +2,8 @@
 //
 //  The ListManager class keeps track of the list of video files to play and the volume settings for
 //  each video.
-// 
+//  For PlayVideo version 2.1
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -10,7 +11,7 @@
 #include <fstream>
 #include <string>
 #include <wiringPi.h>
-
+#include <sstream>
 using namespace std;
 
 #ifndef _VIDEOSPEC_H
@@ -22,9 +23,8 @@ typedef struct videospec {
    int volume;
 } videospec_t;
 
-// Pre-allocated space for video files names. 
+// Pre-allocated space for video files names.
 static const int MAXVIDEOFILES = 300;
-
 #endif
 
 
@@ -33,7 +33,7 @@ static const int MAXVIDEOFILES = 300;
 
 const char LIST_FILE_COMMENT_MARK = '*';
 const char EXTRA_VIDEO_DISK_MARK = '@';
-const char LOOP_VIDEO_MARK = '@';   
+const char LOOP_VIDEO_MARK = '@';
 const string whitespace = " \t";
 
 
@@ -41,14 +41,15 @@ class ListManager {
 
    private:
       string list_filename;  // full path and name of list file
+	  string max_reboot_string; // maximum number of reboot attempts
       videospec_t videos[MAXVIDEOFILES];  // Pre-allocate space
       int last_file_pointer;
       int current_file_pointer;
       string remove_char( string str, char ch);
       string trim (const string str);
-
+	  int RebootCounter(int action, int max_reboot_count);
    public:
-      void initialize(string input_list_filename);
+      void initialize(string input_list_filename, string max_reboot);
       videospec_t currentVideo();
       videospec_t nextVideo();
       videospec_t previousVideo();
